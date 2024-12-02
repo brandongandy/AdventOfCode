1fb195c67c622cbfebf4cb76c20514c68adbdf70
Brandon Gandy <tallain@gmail.com>
2024-12-02 12:33:49 -0800

    day 02

--- AdventOfCode.2024/Day02.cs
+++ AdventOfCode.2024/Day02.cs
@@ -1,4 +1,5 @@
-﻿using AdventOfCode.Y24;
+﻿using AdventOfCode.Utility;
+using AdventOfCode.Y24;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,21 +8,96 @@ using System.Threading.Tasks;
 
 namespace AdventOfCode.Y24;
 
-internal class Day02 : Day
+public class Day02 : Day
 {
-    private long[] Levels { get; }
+    private int[][] Reports { get; }
     public Day02(string input)
     {
-
+        Reports = input
+            .TrimEnd()
+            .ReplaceLineEndings("\n")
+            .Split('\n')
+            .Select(line => line.Split(' ').Select(int.Parse).ToArray())
+            .ToArray();
     }
 
     public override string Part1()
     {
-        throw new NotImplementedException();
+        var safeReports = 0;
+        foreach (var report in Reports)
+        {
+            if (IsReportSafe(report))
+            {
+                safeReports++;
+            }
+        }
+
+        return safeReports.ToString();
     }
 
     public override string Part2()
     {
-        throw new NotImplementedException();
+        var safeReports = 0;
+        foreach (var report in Reports)
+        {
+            if (IsReportSafe(report))
+            {
+                safeReports++;
+                continue;
+            }
+            for (int i = 0; i < report.Length; i++)
+            {
+                // iterate through report, dropping one item at a time
+                // to add some "fault tolerance"
+                var trimmedReport = report.Where((item, index) => index != i).ToArray();
+                if (IsReportSafe(trimmedReport))
+                {
+                    safeReports++;
+                    break;
+                }
+            }
+        }
+
+        return safeReports.ToString();
+    }
+
+    private bool IsReportSafe(int[] report)
+    {
+        var direction = 0;
+        var isReportScaled = true;
+        for (var i = 0; i < report.Length - 1; i++)
+        {
+            var change = report[i].CompareTo(report[i + 1]);
+            if (direction == 0)
+            {
+                direction = change;
+            }
+            else
+            {
+                if (direction != change)
+                {
+                    isReportScaled = false;
+                    break;
+                }
+            }
+        }
+        
+        if (!isReportScaled)
+        {
+            return false;
+        }
+
+        var isReportSmooth = true;
+        for (var i = 0; i < report.Length - 1; i++)
+        {
+            var change = Math.Abs(report[i] - report[i + 1]);
+            if (!change.IsBetween(1, 3))
+            {
+                isReportSmooth = false;
+                break;
+            }
+        }
+
+        return isReportSmooth;
     }
 }
---
+++ AdventOfCode/Utility/MathExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace AdventOfCode.Utility;
+
+public static class MathExtensions
+{
+    public static bool IsBetween<T>(this T item, T min, T max, bool inclusive = true)
+        where T : IComparable<T>
+    {
+        return inclusive 
+            ? min.CompareTo(item) <= 0 && max.CompareTo(item) >= 0
+            : min.CompareTo(item) < 0 && max.CompareTo(item) > 0;
+    }
+}
